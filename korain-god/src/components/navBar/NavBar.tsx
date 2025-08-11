2f328a4d4d280c57f84539719b0709b275ae4e44
Alma <isakvent@outlook.com>
2025-08-11 14:28:10 +0200

    finish navigation bar and behaviour

--- korain-god/src/components/navBar/NavBar.tsx
+++ korain-god/src/components/navBar/NavBar.tsx
@@ -1,21 +1,36 @@
 import { Link } from "react-router-dom";
 import Styles from "./NavBar.module.css";
+import { useRef } from "react";
 
 const NavBar = () => {
+    const linksRef = useRef<HTMLDivElement>(null);
+
+    const scrollLeft = () => {
+        if (linksRef.current) {
+            linksRef.current.scrollBy({ left: -50, behavior: "smooth" });
+        }
+    };
+
+    const scrollRight = () => {
+        if (linksRef.current) {
+            linksRef.current.scrollBy({ left: 50, behavior: "smooth" });
+        }
+    };
+
     return (
         <nav className={Styles.NavBarView}>
-
-
-
-            <div className={Styles.LinksView}>
+            <button className={Styles.ScrollButton} onClick={() => {scrollLeft()}}>←</button>
+            <div className={Styles.LinksView} ref={linksRef}>
+                <hr className={Styles.HeaderRow}/>
                 <Link className={Styles.NavLink} to="/">Home</Link>
-                <p>|</p>
+                <hr className={Styles.HeaderRow}/>
                 <Link className={Styles.NavLink} to="/symbol">Symbol</Link>
                 <Link className={Styles.NavLink} to="/portrait">Portrait</Link>
                 <Link className={Styles.NavLink} to="/shrine">Shrine</Link>
                 <Link className={Styles.NavLink} to="/temple">Temple</Link>
-                <p>|</p>
+                <hr className={Styles.HeaderRow}/>
             </div>
+            <button className={Styles.ScrollButton} onClick={() => {scrollRight()}}>→</button>
 
         </nav>
     )
